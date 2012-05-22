a6316c218ddf6d4e1215779c36f6e23fe699d768
Kari Korpinen <karikkorpinen@gmail.com>
2012-05-22 13:13:35 +0300

    kahden tiedoston haku TreeSetilla

--- Sanasto/src/sanasto/Sanasto.java
+++ Sanasto/src/sanasto/Sanasto.java
@@ -8,6 +8,8 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 /**
  *
@@ -19,8 +21,11 @@ public class Sanasto {
 */
    public static void main(String[] args) {
    // TODO code application logic here
+      TreeSet <String>tree = new TreeSet<String>(); 
+      int rivilaskuri=0;
+      String tiedosto="../Musiciens.txt";
       try{
-         FileInputStream fstream = new FileInputStream("../Musiciens.txt");
+         FileInputStream fstream = new FileInputStream(tiedosto);
          //luodaan streami sisäänluettavalle tiedostolle
          DataInputStream in = new DataInputStream(fstream);
          //luodaan objekti streamille 
@@ -29,26 +34,55 @@ public class Sanasto {
          String strLine;
          while ((strLine = br.readLine()) != null)   {
             //luetaan tiedostoa rivi riviltä
-            System.out.println (strLine);
-            //tulostetaan luettu näkyviin konsoliin
+            rivilaskuri++;
+            //lisätään luettuun riviin tiedostonimi ja rivinumero
+            tree.add("tiedosto "+tiedosto.substring(3)+" rivi "+rivilaskuri+" "+strLine);
+            //tree.add(tiedosto.substring(3)+" "+rivilaskuri+" "+strLine);
          }
          in.close();
          //suljetaan sisäänlukustreami
       }catch (Exception e){//siepataan poikkeus ja yritetään tulostaa virheenaiheuttaja
          System.err.println("Error: " + e.getMessage());
       }   
+      
       try{  //käsitellään toinen tiedosto
-         FileInputStream fstream2 = new FileInputStream("../Berlioz.txt");
+         rivilaskuri=0;; 
+         tiedosto=""; 
+         tiedosto="../Berlioz.txt"; 
+         FileInputStream fstream2 = new FileInputStream(tiedosto);
          DataInputStream in2 = new DataInputStream(fstream2);
          BufferedReader br2 = new BufferedReader(new InputStreamReader(in2));
          String strLine2;
          while ((strLine2 = br2.readLine()) != null)   {
-            System.out.println (strLine2);
+            rivilaskuri++;
+            //tree.add("tiedosto "+tiedosto.substring(3)+" rivi "+rivilaskuri+" "+strLine2);
+            tree.add(tiedosto +" "+rivilaskuri+" "+strLine2);
+            //tree.add(tiedosto.substring(3) +" "+rivilaskuri+" "+strLine2);
          }
          in2.close();
-      }catch (Exception e){//Catch exception if any
-         System.err.println("Error: " + e.getMessage());
-      }   
-   } 
+      }catch (Exception e){
+      System.err.println("Error: " + e.getMessage());
+      }
+      Iterator iterator;
+      iterator = tree.iterator();
+      //haku ei palauta sanoja,joihin kuuluu muita merkkejä,kuten "Beethoven,"tai "-Beethoven" 
+      String haku="Beethoven";
+      while (iterator.hasNext()){    
+         String s = iterator.next().toString();
+         //pilkotaan rivi sanoiksi
+         String[] words2 = s.split(" ");
+         for (String word : words2)//käydään sanat läpi  
+         {
+            if(word.equals(haku))//jos sana on sama kuin hakuarvo,se tulostetaan
+            {
+               System.out.println(s);
+               //Haun tulostus tulee muodossa
+               //Berlioz.txt 9119 directeur des Beaux-Arts s'exprimer ainsi. Il admettait que Beethoven
+               //Musiciens.txt 581 romantisme indompté de Beethoven et de Berlioz, la tragédie du siècle,
+            }   
+         }  
+      }
+      iterator=null;
+   }   
 }
 
